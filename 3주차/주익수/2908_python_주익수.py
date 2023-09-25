46f624a0dacaf5259d8998c5381f916ca76fcf25
dlrtn <dlrtn751@gmail.com>
2023-09-25 11:43:40 +0900

    3주차 주익수 문제 수정

--- 3주차/주익수/2908_python_주익수.py
+++ 3주차/주익수/2908_python_주익수.py
@@ -1,8 +1,8 @@
 import sys
 
-A, B = map(int, sys.stdin.readline().split())
+A, B = map(str, sys.stdin.readline().split()) # A, B를 문자열로 받음, 리버스시키기 위함
 
-reversed_A = int(str(A)[::-1])
-reversed_B = int(str(B)[::-1])
+reversed_A = int(A[::-1]) # 배열을 리버스 시킨 형태를 정수형태로 변환
+reversed_B = int(B[::-1])
 
-print(max(reversed_A, reversed_B))
+print(max(reversed_A, reversed_B)) # 둘 중 큰 값을 출력
