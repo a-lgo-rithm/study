44bc4557bddf08fce31ff92e39ac9830e093767f
dlrtn <dlrtn751@gmail.com>
2023-10-06 10:18:51 +0900

    4주차 주익수 문제 수정

--- 4주차/주익수/14501_python_주익수.py
+++ 4주차/주익수/14501_python_주익수.py
@@ -13,6 +13,5 @@ for i in range(N):
     for j in range(i + schedule[i][0], N + 1):
         if dp[j] < dp[i] + schedule[i][1]:  # 이중 for문을 돌면서 dp[j]에 최대값을 저장한다.
             dp[j] = dp[i] + schedule[i][1]  # dp[j]에는 j일까지의 최대값이 저장된다.
-    print(dp)
 
 print(dp[-1])
